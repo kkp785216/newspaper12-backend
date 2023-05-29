3ea677efc21a53e3177af34b9f44f08c0b049790
Krishna Prajapati <kkp785216@gmail.com>
2023-05-29 15:12:26 +0530

    updated article has implemented

--- src/routes/Admin/Subroute/articleRoute.ts
+++ src/routes/Admin/Subroute/articleRoute.ts
@@ -2,11 +2,15 @@ import express from "express";
 import { addNewArticle } from "../../../controller/Article/addArticleCtrl";
 import { deleteSingleArticle } from "../../../controller/Article/deleteArticleCtrl";
 import { authMiddleware, isAdmin } from "../../../middlewares/authMiddleware";
+import { updateArticle } from "../../../controller/Article/updateArticleCtrl";
 
 const router = express.Router();
 
 // request and route for delete user profile
 router.post("/", authMiddleware, isAdmin, addNewArticle);
+
+// request and route for delete user profile
+router.put("/:slug", authMiddleware, isAdmin, updateArticle);
 
 // request and route for delete user profile
 router.delete("/", authMiddleware, isAdmin, deleteSingleArticle);
